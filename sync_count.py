72143f434747dd78ceb132563ae65151258d57cd
hevienz <hevienz@qq.com>
2020-06-12 12:13:14 +0800

    fix sync_count.py

--- sync_count.py
+++ sync_count.py
@@ -8,42 +8,26 @@ b = BPF(text="""
 BPF_HASH(last);
 
 int do_trace(struct pt_regs *ctx) {
-    u64 ts, *tsp, delta, *count, ckey = 1, key = 0;
+    u64 *count, key = 0;
+    u64 init = 1;
 
-    count = last.lookup(&ckey);
+    count = last.lookup(&key);
     if (count == NULL) {
-        *count = 0;
-    }
-    *count = *count + 1;
-    bpf_trace_printk("COUNT: %d\\n", *count);
-    last.update(&ckey, count);
-
-    // attempt to read stored timestamp
-    tsp = last.lookup(&key);
-    if (tsp != 0) {
-        delta = bpf_ktime_get_ns() - *tsp;
-        if (delta < 1000000000) {
-            // output if time is less than 1 second
-            bpf_trace_printk("%d\\n", delta / 1000000);
-        }
-        last.delete(&key);
+        count = &init;
+        bpf_trace_printk("COUNT: %d\\n", *count);
+        last.update(&key, count);
+    } else {
+        *count = *count + 1;
+        bpf_trace_printk("COUNT: %d\\n", *count);
     }
 
-    // update stored timestamp
-    ts = bpf_ktime_get_ns();
-    last.update(&key, &ts);
     return 0;
 }
 """)
 
 b.attach_kprobe(event=b.get_syscall_fnname("sync"), fn_name="do_trace")
-print("Tracing for quick sync's... Ctrl-C to end")
+print("Tracing for quick sync's count... Ctrl-C to end")
 
-# format output
-start = 0
 while 1:
-    (task, pid, cpu, flags, ts, ms) = b.trace_fields()
-    if start == 0:
-        start = ts
-    ts = ts - start
-    print("At time %.2f s: multiple syncs detected, last %s ms ago" % (ts, ms))
+    (task, pid, cpu, flags, ts, msg) = b.trace_fields()
+    print(task, pid, ts, msg)
